25deef3a8ec74287938fd4c76dbd5efe435dd2fb
Olumide Orija <mrolubanty@gmail.com>
2021-09-14 09:22:17 -0700

    Added User Image

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -93,7 +93,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="{{asset('dist/img/avatar.jpg')}}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>{{Auth::user()->name}}</p>
